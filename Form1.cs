8661bf952d34ea62921bdb4301433f9f88bca23e
Crash122 <38176346+Crash122@users.noreply.github.com>
2018-05-13 13:44:37 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Uranvnenie.Uravnenie(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            label9.Text = Uranvnenie.reshenie1();
+            label10.Text = Uranvnenie.reshenie2();
+            label8.Text = Uranvnenie.otvet();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Uranvnenie.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp15
+{
+    public class Uranvnenie
+    {
+        static public void Uravnenie(double a1, double b1,double c1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+
+            uravn(discrim());
+        }
+        static double a, b, c,x1,x2;
+
+        static bool d =false;
+
+        static private double discrim()
+        {        
+            return Math.Sqrt((b * b) - 4 * (a * c));
+        }
+        
+        static private  double uravnpol(double dis)
+        {
+            
+                return (-b + dis) / (2 * a);
+            
+            
+        }
+
+        static private double uravnotric(double dis)
+        {
+            
+                return (-b - dis) / (2 * a);
+            
+            
+        }
+
+        static void uravn(double dis)
+        {
+           if(dis>=0)
+            {
+                x1=uravnpol(dis);
+                x2=uravnotric(dis);
+            }
+            else
+            {
+                d = true;
+            }
+        }
+        public static string reshenie1()
+        {   double D = b * b;
+            return "D = " + D.ToString() + " - 4 * " + a.ToString() + " * " + c.ToString() + " = " + discrim().ToString();
+        }
+        public static string reshenie2()
+        {   double botric = -b;
+            return "X1,X2 = (" + botric.ToString() + " + - " + discrim().ToString() + ") / (2 * " + a.ToString() + ") = " + x1.ToString() + ";" + x2.ToString();
+        }
+        public static string otvet()
+        {
+            if (d == true)
+            {
+                return "Корней нет";
+            }
+            return "X1 = " + x1.ToString() + ",X1 = " + x2.ToString();
+        }
+
+
+    }
+}
